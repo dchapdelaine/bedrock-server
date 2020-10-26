af3b855f8e2174340cc57d02aa73d976e4fa96eb
David Chapdelaine <david.chapdelaine@gmail.com>
2020-10-26 08:08:26 -0400

    now follow redirect

--- downloadServer.sh
+++ downloadServer.sh
@@ -2,7 +2,7 @@
 
 if [ "$VERSION" = "latest" ] ; then
     LATEST_VERSION=$( \
-        curl -v --silent  https://www.minecraft.net/en-us/download/server/bedrock/ 2>&1 | \
+        curl -v -L --silent  https://www.minecraft.net/en-us/download/server/bedrock/ 2>&1 | \
         grep -o 'https://minecraft.azureedge.net/bin-linux/[^"]*' | \
         sed 's#.*/bedrock-server-##' | sed 's/.zip//')
     export VERSION=$LATEST_VERSION
